e717ccb1336bc0e372a00aa6de69536dbe048926
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-02-05 16:53:58 +0530

    8th

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -2,30 +2,29 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
     public static void main(String args[]) {
-        int n, arg, sum = 0, r;
+
+        int n, temp, sum = 0, r;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("**********\n");
+        System.out.println("**********");
         System.out.print("Enter Number: ");
         n = sc.nextInt();
 
-        arg = n;
-        for (int i = 1; i < n; i++) {
-            while (n > 0) {
-                r = n % 10;
-                sum = sum + (r * r * r);
-                n = n / 10;
+        temp = n; // store original number
 
-            }
-
+        while (n > 0) {
+            r = n % 10;
+            sum = sum + (r * r * r);
+            n = n / 10;
         }
-        if (arg == sum) {
-            System.out.println("Given number is armstrong number: " + arg);
+
+        if (temp == sum) {
+            System.out.println("Given number is Armstrong number: " + temp);
         } else {
-            System.out.println("Given number is not armstrong number: " + arg);
+            System.out.println("Given number is not Armstrong number: " + temp);
         }
-        System.out.println("\n**********");
+
+        System.out.println("**********");
         sc.close();
     }
-
 }
