575e9e812eb6f8a1f2f7330b47bb2a684db2ddc2
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-02-05 13:35:23 +0530

    2nd

---
+++ oddSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class oddSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int sum = 0;
+        System.out.print("Enter Number(Length): ");
+        num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println("Total : " + sum);
+
+    }
+}
