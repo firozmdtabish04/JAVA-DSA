9673d12b98748e7506fd4cc61239f0007a10a477
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-02-14 18:14:57 +0530

    a

--- pattern/square.java
+++ pattern/square.java
@@ -1,5 +1,16 @@
 package pattern;
 
 public class square {
-    
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                System.out.print("* ");
+                // count++;
+                // System.out.print(count +" ");
+            }
+            System.out.println(); // use to move cursor in next line
+        }
+        
+    }
 }
