3bb3e219a1f3c57716a94c15ef989f5a288d5879
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-02-15 21:38:57 +0530

    more

---
+++ Programs/Arithmeticope.java
@@ -0,0 +1,10 @@
+package Programs;
+public class Arithmeticope {
+    public static void main(String[] args) {
+       
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(5+15/3*2-8%3);
+        System.out.println((55+9)%9);
+    }
+}
