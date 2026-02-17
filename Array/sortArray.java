8d69f505168f5e2d1cd92f6246d20d048308323a
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-02-17 23:44:52 +0530

    All button work successfully

--- Array/sortArray.java
+++ Array/sortArray.java
@@ -1,5 +1,11 @@
 package Array;
 
+import java.util.Arrays; 
+
 public class sortArray {
-    
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 10, 30, 20, 40, 50 };
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
