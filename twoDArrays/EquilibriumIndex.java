7ac721da2e2c135e7ad361cd8d253ff2c7c4dfcd
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-05 09:42:49 +0530

    done

--- twoDArrays/EquilibriumIndex.java
+++ twoDArrays/EquilibriumIndex.java
@@ -1,12 +1,18 @@
 package twoDArrays;
+
 public class EquilibriumIndex {
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 5, 2, 2 };
+        int arr[] = { 8, 3, 2,2, 5, 2, 9, 2 ,2};
+        EquilibriumIdx(arr);
+
+    }
+
+    public static void EquilibriumIdx(int arr[]) {
         int totalSum = 0;
+        int leftSum = 0;
         for (int i = 0; i < arr.length; i++) {
             totalSum += arr[i];
         }
-        int leftSum = 0;
         for (int i = 0; i < arr.length; i++) {
             totalSum = totalSum - arr[i]; // right sum
             if (leftSum == totalSum) {
--- twoDArrays/majorityEle.java
+++ twoDArrays/majorityEle.java
@@ -1,5 +1,30 @@
 package twoDArrays;
 
+import java.util.Arrays;
+
 public class majorityEle {
-    
-}
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 2, 1, 3, 3, 2, 2 ,3,2,3,2};
+        int candidate = arr[0];
+        int count = 1;
+        System.out.println("Length of array: " + arr.length);
+        System.out.println("Array elements arr: ");
+        // Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+", ");
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                candidate = arr[i];
+                count = 1;
+            }
+        }
+        System.out.println("\nMajority Element: " + candidate);
+    }
+} 
