858999861bf97fccb7b5ee67b198a3244f357980
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-05 09:44:14 +0530

    done

--- pattern/square.java
+++ pattern/square.java
@@ -2,7 +2,7 @@ package pattern;
 
 public class square {
     public static void main(String[] args) {
-        int count = 0;
+        // int count = 0;
         for (int i = 1; i < 5; i++) {
             for (int j = 1; j < 5; j++) {
                 System.out.print("* ");
