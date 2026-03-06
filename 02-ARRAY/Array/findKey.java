e08a7eef65d7712cce2c4f0203b63345c9c742b8
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-07 01:05:02 +0530

    added

--- 02-ARRAY/Array/findKey.java
+++ 02-ARRAY/Array/findKey.java
@@ -3,16 +3,13 @@
 public class findKey {
     public static void main(String[] args) {
         int a[] = new int[] { 12, 44, 23, 56, 9, 23, 78, 13 };
-        int key = 560;
+        int key = 44;
         int index = 0;
         for (int i = 0; i < a.length; i++) {
 
             if (a[i] == key) {
                 index=i;
                 System.out.println("Key found : " + key +" Index: "+index);
-            } else {
-                System.out.println("Sorry no match...");
-                return;
             }
         }
         
