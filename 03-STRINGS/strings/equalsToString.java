ed10fb1c4962ee3659b8cbbf2f24519982c7214d
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-10 00:07:59 +0530

    Strings

--- 03-STRINGS/strings/equalsToString.java
+++ 03-STRINGS/strings/equalsToString.java
@@ -10,9 +10,9 @@ public class equalsToString {
         // System.out.println(str.equalsIgnoreCase(str1));
         // System.out.println(str.compareTo(str1));
         // System.out.println(str.concat(str1));
-        // System.out.println(str2.isEmpty());
+        System.out.println(str2.isEmpty());
         // System.out.println(str1.isBlank());
-        // System.out.println(str1.endsWith(str));
+        System.out.println(str1.endsWith(str));
         System.out.println(str1.codePointAt(0));
     }
 }
