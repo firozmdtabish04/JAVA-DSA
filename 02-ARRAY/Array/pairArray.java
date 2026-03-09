a93442141f88589a59df8f3209cb8f420bbfa9c0
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-10 00:08:27 +0530

    Strings

---
+++ 02-ARRAY/Array/pairArray.java
@@ -0,0 +1,22 @@
+public class pairArray {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 6 };
+        pairArr(arr);
+    }
+
+    public static void pairArr(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + curr + ", " + arr[j] + ") ");
+                total++;
+            }
+            System.out.println();
+
+        }
+        System.out.println("Total pairs: " + total);
+    }
+}
+
+// n(n-1)/2 = total pairs
