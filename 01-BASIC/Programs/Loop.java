d5ff9bacb7aa27ed392152ee025c6674ea70ec6b
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-04-27 22:37:00 +0530

    add

---
+++ 01-BASIC/Programs/Loop.java
@@ -0,0 +1,13 @@
+public class Loop {
+    public static void main(String[] args) {
+        int num = 10;
+        int i = 0;
+        // for (int i = 0; i <= num; i++) {
+        // System.out.print(i + ", ");
+        // }
+        while (num > i) {
+            System.out.print(i + ", ");
+            i++;
+        }
+    }
+}
