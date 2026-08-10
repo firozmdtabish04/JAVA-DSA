01bad7670209055f78d8ee034c4f8e03c47a9e7e
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-08-10 22:57:54 +0530

    Pattern Revision

--- 01-BASIC/pattern/alphabet.java
+++ 01-BASIC/pattern/alphabet.java
@@ -3,20 +3,18 @@ public class alphabet {
     public static void main(String[] args) {
         int num = 10;
         char ch = 'A';
-
+        charPattern(num, ch);
+    }
+    public static void charPattern(int num , char ch) {
         for (int i = 1; i <= num; i++) {
-
             for (int j = 1; j <= i; j++) {
-
                 if (ch > 'z' || ch > 'Z') {
                     System.out.println("\n\nExit!\n");
                     return;
-                }
-
+                }  
                 System.out.print(ch + " ");
                 ch++;
             }
-
             System.out.println();
         }
     }
---
+++ 01-BASIC/pattern/revisionPattern.java
@@ -0,0 +1,80 @@
+public class revisionPattern {
+    
+    public static void main(String[] args) {
+        int n =5;
+        int m =3;
+
+
+        // Square Print
+        /*
+        * * * *
+        * * * *
+        * * * *
+        * * * *
+        */ 
+        // for(int i =0; i<n; i++){
+        //     for(int j =0; j<n; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Right Angle triangle
+        /*
++ 
++ + 
++ + + 
++ + + +
+        */ 
+        // for(int i =n-1; i>0; i--){
+        //     for (int j =i; j<n; j++){
+        //         System.out.print("+ ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Inverted Right Angle triangle
+        /*
+* * * * 
+* * * 
+* * 
+* 
+        */ 
+        // for(int i =0; i<n; i++){
+        //     for(int j =i; j<n; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Rectangle Pattern
+        /*
+* * * 
+* * *
+        */ 
+        // for(int i =0; i<n; i++){
+        //     for (int j =0; j<m; j++){
+        //         System.out.print("* ");
+        //     } 
+        //     System.out.println();
+        // }
+
+        // Hollow Rectangle
+
+
+
+
+        // for (int i=n-1; i>0; i--){
+        //     for(int j=i; j<n; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i=0; i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
